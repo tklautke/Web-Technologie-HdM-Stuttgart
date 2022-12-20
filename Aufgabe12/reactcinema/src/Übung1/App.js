6a9a8d880a1f8e9335fa31c8b2641a011513e845
Theo Klautke <theoklautkemail@gmail.com>
2022-12-20 20:21:45 +0100

    Added "Aufgabe12"

---
+++ Aufgabe12/reactcinema/src/Übung1/App.js
@@ -0,0 +1,17 @@
+import logo from '../logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="headliner" >Es geht los!</p>
+          <p>
+              Der erste Schritt ist geschafft.
+          </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
